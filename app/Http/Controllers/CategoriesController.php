31546f90f7899b11c11ad9d04aadcb8e637924b1
Shai Gerbi <ShaiGerbi@gmail.com>
2021-08-26 21:52:07 +0300

    create controllers

---
+++ app/Http/Controllers/CategoriesController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Resources\CategoryResource;
+use App\Models\Category;
+
+class CategoriesController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $categories = Category::with(['products'])->get();
+        return CategoryResource::collection($categories);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Category  $category
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Category $category)
+    {
+        return new CategoryResource($category->loadMissing(['products']));
+    }
+}
---
+++ app/Http/Controllers/ProductsController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Resources\ProductResource;
+use App\Models\Product;
+
+class ProductsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $products = Product::with(['category'])->get();
+        return ProductResource::collection($products);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Product  $product
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Product $product)
+    {
+        return new ProductResource($product->loadMissing(['category']));
+    }
+}
